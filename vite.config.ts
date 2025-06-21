275a13a288fa2dbb3ea8879914a64db608f8a103
Thiri Win <thiriwinzhuang@gmail.com>
2025-06-21 20:20:07 +0630

    remove build from vite

--- vite.config.ts
+++ vite.config.ts
@@ -28,9 +28,4 @@ export default defineConfig({
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        emptyOutDir: true,
-    },
 });
