45ab024c5a2c160c5d33417769214e9cd05b8cd5
Thiri Win <thiriwinzhuang@gmail.com>
2025-06-25 21:35:52 +0630

    some frontend view changed

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,10 +3,10 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+    <div class="flex aspect-square size-9 items-center justify-center rounded-md text-sidebar-primary-foreground ">
+        <AppLogoIcon class="size-5 fill-current text-white dark:text-black "/>
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Laravel Starter Kit</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">Htoo Automobile Service</span>
     </div>
 </template>
