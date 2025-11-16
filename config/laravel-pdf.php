59a3e177d58559b79838c3513085b9ef15e7d07c
Thiri Win <thiriwinzhuang@gmail.com>
2025-11-16 18:57:28 +0630

    test again for pdf

--- config/laravel-pdf.php
+++ config/laravel-pdf.php
@@ -11,12 +11,12 @@ return [
          * Configure the paths to Node.js, npm, Chrome, and other binaries.
          * Leave null to use system defaults or Browsershot's auto-detection.
          */
-        'node_binary' => env('LARAVEL_PDF_NODE_BINARY'),
-        'npm_binary' => env('LARAVEL_PDF_NPM_BINARY'),
+        'node_binary' => env('LARAVEL_PDF_NODE_BINARY', '/var/www/.nvm/versions/node/22/bin/node'),
+        'npm_binary' => env('LARAVEL_PDF_NPM_BINARY', '/var/www/.nvm/versions/node/22/bin/npm'),
         'include_path' => env('LARAVEL_PDF_INCLUDE_PATH'),
         'chrome_path' => env('LARAVEL_PDF_CHROME_PATH'),
-        'node_modules_path' => env('LARAVEL_PDF_NODE_MODULES_PATH'),
-        'bin_path' => env('LARAVEL_PDF_BIN_PATH'),
+        'node_modules_path' => env('LARAVEL_PDF_NODE_MODULES_PATH', base_path('node_modules')),
+        'bin_path' => env('LARAVEL_PDF_BIN_PATH', base_path('vendor/bin')),
         'temp_path' => env('LARAVEL_PDF_TEMP_PATH'),
 
         /*
