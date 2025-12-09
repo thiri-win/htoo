6183a22ecd4c51c47e53e5e4f27d9a2b5ff5748e
Thiri Win <thiriwinzhuang@gmail.com>
2025-12-09 10:27:31 +0630

    Add date and subject information to quotation header

--- resources/views/partials/_quotationheader.blade.php
+++ resources/views/partials/_quotationheader.blade.php
@@ -80,4 +80,17 @@
 
     <h1>Quotation</h1>
 
+    <div style="display:flex; justify-content: space-between;">
+            <div>
+                <label>Date:</label>
+        <span>$data->date</span>
+    <br>
+        <label>Subject:</label>
+        <span>$data->subject</span>
+    </div>
+        <div>
+        <label>To:</label>
+        <p>$data->to</p>
+    </div>
+
 </header>
