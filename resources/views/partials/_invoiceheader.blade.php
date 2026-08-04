673857d29afd24a94dff00b0c889fd82b1416be0
Thiri Win <thiriwinzhuang@gmail.com>
2026-08-04 11:38:50 +0630

    edit for myanmar font in invoice header 2

--- resources/views/partials/_invoiceheader.blade.php
+++ resources/views/partials/_invoiceheader.blade.php
@@ -1,148 +1,161 @@
-<style>
-    @font-face {
-        font-family: 'poppins';
-        src: url('{{ public_path('fonts/Poppins/Poppins-Medium.ttf') }}');
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    @font-face {
-        font-family: 'myanmar';
-        src: url('{{ public_path('fonts/Noto_Sans_Myanmar/NotoSansMyanmar-Medium.ttf') }}') format('truetype');
-    }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        @font-face {
+            font-family: 'poppins';
+            src: url('{{ public_path('fonts/Poppins/Poppins-Medium.ttf') }}');
+        }
 
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        line-height: 1.5;
-        font-size: 14px;
-        font-family: 'myanmar' !important;
-        font-weight: 400;
-    }
+        @font-face {
+            font-family: 'myanmar';
+            src: url('{{ public_path('fonts/Noto_Sans_Myanmar/NotoSansMyanmar-Medium.ttf') }}') format('truetype');
+        }
 
-    .header {
-        width: 100%;
-        padding: 10mm;
-    }
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            line-height: 1.5;
+            font-size: 14px;
+            font-family: 'myanmar' !important;
+            font-weight: 400;
+        }
 
-    .container {
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
+        .header {
+            width: 100%;
+            padding: 10mm;
+        }
 
-    .container img,
-    .container .content {
-        text-align: right;
-    }
+        .container {
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
 
-    h1 {
-        text-align: center;
-        color: #1b5f21;
-        text-transform: uppercase;
-        margin: 12px 0;
-        font-size: 30px;
-        font-weight: 700;
-        font-family: "poppins";
-    }
+        .container img,
+        .container .content {
+            text-align: right;
+        }
 
-    h2 {
-        font-family: "poppins";
-        font-weight: 600;
-        font-size: 16px;
-    }
+        h1 {
+            text-align: center;
+            color: #1b5f21;
+            text-transform: uppercase;
+            margin: 12px 0;
+            font-size: 30px;
+            font-weight: 700;
+            font-family: "poppins";
+        }
 
-    td {
-        padding: 5px;
-        white-space: nowrap;
+        h2 {
+            font-family: "poppins";
+            font-weight: 600;
+            font-size: 16px;
+        }
 
-    }
+        td {
+            padding: 5px;
+            white-space: nowrap;
 
-    .content p,
-    .content strong {
-        font-size: 12px;
-    }
-</style>
+        }
 
-<header class="header">
-    <div class="container">
-        <div>
-            <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('image/htoo_logo_green.png'))) }}" alt="" style="height: 150px;">
+        .content p,
+        .content strong {
+            font-size: 12px;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <div>
+                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('image/htoo_logo_green.png'))) }}" alt="" style="height: 150px;">
+            </div>
+            <div class="content">
+                <h2>Htoo Automobile Service</h2>
+                <p>(29/B), Shwe Ta Gar St, Bon Shay Gone Ward</p>
+                <p>Shwe Pyi Thar Township, Yangon</p>
+                <p>&#9742; 09 443067898, &#9742;09 795355487</p>
+                <p style="font-family: poppins;">&blacktriangleright; UAB Bank -
+                    <strong style="font-family: poppins;">0243 3010 0011 287</strong>
+                    [<strong>AUNG KO HEIN</strong>]
+                </p>
+                <p style="font-family: poppins;">&blacktriangleright; KBZ Bank -
+                    <strong style="font-family: poppins;">1225 1105 7008 30201</strong>
+                    [<strong>THIRI WIN</strong>]
+                </p>
+                <p style="font-family: poppins;">&blacktriangleright; AYA Bank -
+                    <strong style="font-family: poppins;">2000 4254 047</strong>
+                    [<strong>THIRI WIN</strong>]
+                </p>
+                <p style="font-family: poppins;">&blacktriangleright; KBZ Pay -
+                    <strong style="font-family: poppins;">09443067898</strong>
+                    [<strong>AUNG KO HEIN</strong>]
+                </p>
+            </div>
         </div>
-        <div class="content">
-            <h2>Htoo Automobile Service</h2>
-            <p>(29/B), Shwe Ta Gar St, Bon Shay Gone Ward</p>
-            <p>Shwe Pyi Thar Township, Yangon</p>
-            <p>&#9742; 09 443067898, &#9742;09 795355487</p>
-            <p style="font-family: poppins;">&blacktriangleright; UAB Bank -
-                <strong style="font-family: poppins;">0243 3010 0011 287</strong>
-                [<strong>AUNG KO HEIN</strong>]
-            </p>
-            <p style="font-family: poppins;">&blacktriangleright; KBZ Bank -
-                <strong style="font-family: poppins;">1225 1105 7008 30201</strong>
-                [<strong>THIRI WIN</strong>]
-            </p>
-            <p style="font-family: poppins;">&blacktriangleright; AYA Bank -
-                <strong style="font-family: poppins;">2000 4254 047</strong>
-                [<strong>THIRI WIN</strong>]
-            </p>
-            <p style="font-family: poppins;">&blacktriangleright; KBZ Pay -
-                <strong style="font-family: poppins;">09443067898</strong>
-                [<strong>AUNG KO HEIN</strong>]
-            </p>
-        </div>
-    </div>
 
-    <h1>Invoice</h1>
+        <h1>Invoice</h1>
 
-    <table style="width: 100%;">
-        <tbody>
-            <tr>
-                <td style="font-size: 12px;"></td>
-                <td style="font-size:12px; width:16%">
-                </td>
+        <table style="width: 100%;">
+            <tbody>
+                <tr>
+                    <td style="font-size: 12px;"></td>
+                    <td style="font-size:12px; width:16%">
+                    </td>
 
-                <td style="font-size: 12px;"></td>
-                <td style="font-size:12px; width:16%">
-                </td>
+                    <td style="font-size: 12px;"></td>
+                    <td style="font-size:12px; width:16%">
+                    </td>
 
-                <td style="font-size: 12px;">Invoice No:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
-                    {{ $data->record_number ?? '' }}
-                </td>
-            </tr>
-            <tr>
-                <td style="font-size: 12px;">Date:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
-                    {{ date_format($data->date, 'd-M-Y') }}
-                </td>
+                    <td style="font-size: 12px;">Invoice No:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
+                        {{ $data->record_number ?? '' }}
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-size: 12px;">Date:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
+                        {{ date_format($data->date, 'd-M-Y') }}
+                    </td>
 
-                <td style="font-size: 12px;">Customer Name:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
-                    {{ $data->car->customer_name ?? '' }}
-                </td>
+                    <td style="font-size: 12px;">Customer Name:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
+                        {{ $data->car->customer_name ?? '' }}
+                    </td>
 
-                <td style="font-size: 12px;">Customer Phone:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
-                    {{ $data->car->customer_phone ?? '' }}
-                </td>
-            </tr>
-            <tr>
-                <td style="font-size: 12px;">Car Number:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
-                    {{ $data->car->car_number ?? '' }}
-                </td>
+                    <td style="font-size: 12px;">Customer Phone:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
+                        {{ $data->car->customer_phone ?? '' }}
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-size: 12px;">Car Number:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; width:16%">
+                        {{ $data->car->car_number ?? '' }}
+                    </td>
 
-                <td style="font-size: 12px;">Car Brand:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; min-width: 65px;">
-                    {{ $data->car->car_brand ?? '' }}
-                </td>
+                    <td style="font-size: 12px;">Car Brand:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; min-width: 65px;">
+                        {{ $data->car->car_brand ?? '' }}
+                    </td>
 
-                <td style="font-size: 12px;">Car Model:</td>
-                <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; min-width: 65px;">
-                    {{ $data->car->car_model ?? '' }}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</header>
+                    <td style="font-size: 12px;">Car Model:</td>
+                    <td style="border-bottom: 1px solid #f1f2f3; font-size:12px; min-width: 65px;">
+                        {{ $data->car->car_model ?? '' }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </header>
+</body>
+
+</html>
